0a13be3042da4f4aac01fe62d99d36ad34fca19b
ItsDayMoon <playsprofile@gmail.com>
2025-09-20 20:45:51 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 
-#define PIR_PIN 4   // GPIO onde o PIR está conectado
-#define LED_PIN 2   // GPIO do LED (no ESP32-S3 geralmente é o 2)
+#define PIR_PIN 4   
+#define LED_PIN 2   
 
 void setup() {
   pinMode(PIR_PIN, INPUT);
@@ -20,5 +20,5 @@ void loop() {
     digitalWrite(LED_PIN, LOW);
   }
 
-  delay(200); // Evita poluir o Serial
+  delay(200); 
 }
